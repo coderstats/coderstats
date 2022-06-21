0eba25ebfcf6019d1e1963234b74264eda426f5c
Ramiro Gómez <code@ramiro.org>
2022-06-21 02:41:36 +0200

    Change deploy source

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 deploy_repo=~/repos/deploy/coderstats.github.io
 
 logya gen
-cp -r deploy/* "$deploy_repo"
+cp -r public/* "$deploy_repo"
 cd "$deploy_repo"
 git add .
 git commit -am'new deployment'
